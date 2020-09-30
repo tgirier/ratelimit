cc73cfa22793b03ec6e65f53b8b8316a3c692a55
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-09-30 19:23:37 +0200

    Add worker to make single request

--- polok.go
+++ polok.go
@@ -1,3 +1,29 @@
 // Package polok implements API testing tool
 // It enables load testing with rate-limiting and burst functionalities
 package polok
+
+import (
+	"net/http"
+)
+
+// Worker is responsible for requesting a given URL with a given method
+type Worker struct{}
+
+// Request makes a request to a given URL with a given method
+func (w *Worker) Request(method string, url string) (int, error) {
+	c := http.Client{}
+
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return 0, err
+	}
+
+	sc := resp.StatusCode
+
+	return sc, nil
+}
--- polok_test.go
+++ polok_test.go
@@ -1,1 +1,24 @@
 package polok_test
+
+import (
+	"testing"
+
+	"github.com/tgirier/polok"
+)
+
+func TestRequest(t *testing.T) {
+	method := "GET"
+	url := "http://www.google.com"
+	want := 200
+
+	w := polok.Worker{}
+
+	got, err := w.Request(method, url)
+	if err != nil {
+		t.Fatalf("worker - %v", err)
+	}
+
+	if got != want {
+		t.Fatalf("worker - got %d, want %d", got, want)
+	}
+}
