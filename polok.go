c8920cf99118ad4c02eb763ee2fd525bef671786
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-11-23 19:10:20 +0100

    Delete unnecessary code due to refactoring

--- polok.go
+++ polok.go
@@ -3,7 +3,6 @@
 package polok
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"sync"
@@ -23,31 +22,6 @@ func Limit(rate float64, bucket <-chan struct{}) {
 			<-bucket
 		}
 	}()
-}
-
-// Request makes a given request if it is able to post a token.
-// A custom http client can be provided, otherwise http default client will be used
-func Request(req *http.Request, client *http.Client, bucket chan<- struct{}, reporting chan<- *http.Response, wg *sync.WaitGroup) error {
-	if client == nil {
-		client = http.DefaultClient
-	}
-
-	if req == nil {
-		return errors.New("missing request")
-	}
-
-	bucket <- struct{}{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-
-	reporting <- resp
-
-	wg.Done()
-
-	return nil
 }
 
 // NewRequester makes HTTP requests.
@@ -152,60 +126,4 @@ func (p *Pipeline) Do(done <-chan struct{}, inputStream <-chan *http.Request) (r
 	}()
 
 	return resultStream
-}
-
-// Requests makes a given list of requests.
-// It launches as many Request goroutines as they are requests provided.
-// It calculates the overall rate of the requests.
-func Requests(requests []*http.Request, client *http.Client, bucket chan<- struct{}, reporting chan<- *http.Response) (rate float64, err error) {
-	var wg sync.WaitGroup
-
-	wg.Add(len(requests))
-
-	start := time.Now()
-
-	for _, req := range requests {
-		go Request(req, client, bucket, reporting, &wg)
-	}
-
-	wg.Wait()
-
-	stop := time.Now()
-	duration := stop.Sub(start).Seconds()
-
-	rate = float64(len(requests)) / duration
-
-	return rate, nil
-}
-
-// Report tracks the progress of given number of requests
-// It aggregates the results within a list of responses
-func Report(number int, reporting <-chan *http.Response) (responses []*http.Response) {
-	var resp []*http.Response
-
-	for i := 0; i < number; i++ {
-		r := <-reporting
-		resp = append(resp, r)
-	}
-
-	return resp
-}
-
-// RequestWithLimit launches a given number of requests to a given URL at a given rate.
-// A custom http client can be provided, otherwise http default client will be used.
-func RequestWithLimit(requests []*http.Request, rate float64, client *http.Client) (responses []*http.Response, finalRate float64, err error) {
-
-	tokens := make(chan struct{})
-	reporting := make(chan *http.Response, len(requests))
-
-	go Limit(rate, tokens)
-
-	rate, err = Requests(requests, client, tokens, reporting)
-	if err != nil {
-		return []*http.Response{}, 0, err
-	}
-
-	resp := Report(len(requests), reporting)
-
-	return resp, rate, nil
 }
--- polok_test.go
+++ polok_test.go
@@ -2,54 +2,13 @@ package polok_test
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"strings"
-	"sync"
 	"testing"
 	"time"
 
 	"github.com/tgirier/polok"
 )
-
-func TestRequest(t *testing.T) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World")
-	}))
-	defer ts.Close()
-
-	method := "GET"
-	url := ts.URL
-	want := http.StatusOK
-	var wg sync.WaitGroup
-
-	bucket := make(chan struct{}, 1)
-	reporting := make(chan *http.Response, 1)
-
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		t.Fatalf("worker - %v", err)
-	}
-
-	wg.Add(1)
-
-	err = polok.Request(req, ts.Client(), bucket, reporting, &wg)
-	if err != nil {
-		t.Fatalf("worker - %v", err)
-	}
-
-	resp := <-reporting
-
-	got := resp.StatusCode
-
-	if got != want {
-		t.Fatalf("worker - got %d, want %d", got, want)
-	}
-	if len(bucket) == 0 {
-		t.Fatalf("worker - remaining tokens in bucket %v", len(bucket))
-	}
-}
 
 func TestNewRequest(t *testing.T) {
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -168,77 +127,6 @@ func TestPipelineDo(t *testing.T) {
 	}
 	if rate > expectedRate {
 		t.Fatalf("rate %v, expected %v", rate, expectedRate)
-	}
-}
-
-func TestReport(t *testing.T) {
-	n := 2
-	reporting := make(chan *http.Response, n)
-	want := "Hello World !"
-
-	for i := 0; i < n; i++ {
-		resp, err := newResponse()
-		if err != nil {
-			t.Fatal(err)
-		}
-		reporting <- resp
-	}
-
-	responses := polok.Report(n, reporting)
-
-	for _, resp := range responses {
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer resp.Body.Close()
-
-		got := strings.TrimSuffix(string(body), "\n")
-
-		if got != want {
-			t.Fatalf("got %s, want %s", got, want)
-		}
-	}
-
-	if len(responses) != n {
-		t.Fatalf("expected %d responses, got %d", n, len(responses))
-	}
-
-}
-
-func TestRequestWithLimit(t *testing.T) {
-	initialRate := 100.0
-	requestNumber := 20
-	var requests []*http.Request
-
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World !")
-	}))
-	defer ts.Close()
-
-	for i := 0; i < requestNumber; i++ {
-		req, err := http.NewRequest("GET", ts.URL, nil)
-		if err != nil {
-			t.Fatalf("request - %v", err)
-		}
-		requests = append(requests, req)
-	}
-
-	resp, rate, err := polok.RequestWithLimit(requests, initialRate, ts.Client())
-	if err != nil {
-		t.Fatalf("request with limit - %v", err)
-	}
-
-	got := len(resp)
-	expectedRate := initialRate
-	want := requestNumber
-
-	if got != want {
-		t.Fatalf("request - got %v requests, want %v", got, want)
-	}
-	if rate > expectedRate {
-		t.Fatalf("request - rate %v, expected %v", rate, expectedRate)
 	}
 }
 
