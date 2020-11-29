4ce0b0662264a28213fb71e386643e37ce673aa9
Thibaut Girier <30323597+tgirier@users.noreply.github.com>
2020-11-29 18:04:12 +0100

    Refactor design into an http client

--- polok.go
+++ polok.go
@@ -3,144 +3,60 @@
 package polok
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 )
 
-// Pipeline represents a data pipeline that executes a given function on a given dataset.
-// The pipeline execution can be rate limited.
-type Pipeline struct {
-	Rate         float64       // Rate at which the pipeline should be limited
-	WorkerNumber int           // Number of workers to spin up to execute the given functionality
-	Client       *http.Client  // Client to use for http calls
-	done         chan struct{} // Done channel triggering pipleine cancellation
+// RateLimitedHTTPClient is an HTTP client that rate limits requests.
+// If the rate is not specified, it defaults to a plain HTTP client.
+type RateLimitedHTTPClient struct {
+	Transport         http.RoundTripper
+	CheckRedirect     func(req *http.Request, via []*http.Request) error
+	Jar               http.CookieJar
+	Timeout           time.Duration
+	Rate              float64
+	ticker            *time.Ticker
+	tickerInitialized bool
+	clientInitialized bool
+	client            http.Client
 }
 
-// Run spins up a pipeline and launches the execution
-// Closing the done channel cancel the whole pipeline
-func (p *Pipeline) Run(inputStream <-chan *http.Request) (responseStream <-chan *http.Response) {
-	resultStream := make(chan *http.Response)
-	p.done = make(chan struct{})
+// Get issues a request.
+func (c *RateLimitedHTTPClient) Get(url string) (*http.Response, error) {
+	c.initializeTicker()
+	c.initializeClient()
 
-	go func() {
-		bucket := make(chan struct{})
-
-		var workerResultSteams []<-chan *http.Response
-
-		for i := 0; i < p.WorkerNumber; i++ {
-			result := p.do(inputStream, bucket)
-			workerResultSteams = append(workerResultSteams, result)
-		}
-
-		p.limit(bucket)
-
-		mergedStream := p.responseStreamsMerge(workerResultSteams...)
-
-		for {
-			select {
-			case <-p.done:
-				return
-			case res := <-mergedStream:
-				resultStream <- res
-			}
-		}
-
-	}()
-
-	return resultStream
-}
-
-// Stop cancels a running pipeline.
-// It closes the internal done channel of the pipeline.
-// It cancels all goroutines belonging to this pipeline.
-func (p *Pipeline) Stop() {
-	close(p.done)
-}
-
-// Limit is responsible for enforcing a global rate limiting expressed in requests per seconds
-// Limit consumes a given number of tokens from the bucket channel at a given rate.
-// A token represents a single request.
-func (p *Pipeline) limit(bucket <-chan struct{}) {
-	go func() {
-		tickInterval := time.Duration(1e9/p.Rate) * time.Nanosecond
-		tick := time.Tick(tickInterval)
-
-		for range tick {
-			<-bucket
-		}
-	}()
-}
-
-// Do makes HTTP requests.
-// It listens to an incoming stream of requests and return a channel of corresponding responses.
-func (p *Pipeline) do(inputStream <-chan *http.Request, bucket chan<- struct{}) (responseStream <-chan *http.Response) {
-	resultStream := make(chan *http.Response)
-
-	client := p.Client
-	if client == nil {
-		client = http.DefaultClient
+	if c.ticker != nil {
+		<-c.ticker.C
 	}
 
-	go func() {
-		for {
-			select {
-			case <-p.done:
-				return
-			case req := <-inputStream:
-				bucket <- struct{}{}
-				res, err := client.Do(req)
-				if err != nil {
-					fmt.Printf("request failed, %v", err)
-				}
-				resultStream <- res
-			}
-		}
-	}()
-	return resultStream
+	return c.client.Get(url)
 }
 
-// responseStreamsMerge merges multiple response streams into one single stream
-func (p *Pipeline) responseStreamsMerge(inputStreams ...<-chan *http.Response) (responseStream <-chan *http.Response) {
-	multiplexedStream := make(chan *http.Response)
-
-	multiplex := func(stream <-chan *http.Response) {
-		for {
-			select {
-			case <-p.done:
-				return
-			case res := <-stream:
-				multiplexedStream <- res
-			}
-		}
+// initializeTicker sets up a client time ticker base on the provided rate.
+func (c *RateLimitedHTTPClient) initializeTicker() {
+	if c.tickerInitialized {
+		return
 	}
-
-	for _, stream := range inputStreams {
-		go multiplex(stream)
+	if c.ticker == nil && c.Rate != 0.0 {
+		tickInterval := time.Duration(1e9/c.Rate) * time.Nanosecond
+		c.ticker = time.NewTicker(tickInterval)
 	}
-
-	return multiplexedStream
+	c.tickerInitialized = true
 }
 
-// SlicetoStream transforms a slice of value into an input stream
-// It enables to input a slice of value into a pipeline
-func SlicetoStream(inputs []*http.Request) (generatedStream <-chan *http.Request) {
-	outStream := make(chan *http.Request)
-	go func() {
-		for _, req := range inputs {
-			outStream <- req
-		}
-	}()
-	return outStream
-}
-
-// StreamtoSlice transforms an outputStream into a slice of values
-// It enables to collect a define number of values out of a pipeline.
-func StreamtoSlice(outputStream <-chan *http.Response, numberToTake int) (output []*http.Response) {
-	var outputSlice []*http.Response
-	for i := 0; i < numberToTake; i++ {
-		res := <-outputStream
-		outputSlice = append(outputSlice, res)
+// initializeClient configures the embedded http client based on specified propoerties.
+func (c *RateLimitedHTTPClient) initializeClient() {
+	if c.clientInitialized {
+		return
 	}
-	return outputSlice
+
+	c.client = http.Client{
+		Transport:     c.Transport,
+		CheckRedirect: c.CheckRedirect,
+		Jar:           c.Jar,
+		Timeout:       c.Timeout,
+	}
+
+	c.clientInitialized = true
 }
--- polok_test.go
+++ polok_test.go
@@ -4,134 +4,57 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/tgirier/polok"
 )
 
-func TestPipelineRun(t *testing.T) {
+func TestGet(t *testing.T) {
 	t.Parallel()
-	expectedRate := 1.0
-	numWorkers := 2
-	numRequests := 3
-
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World!")
-	}))
-
-	method := "GET"
-	url := ts.URL
-	want := http.StatusOK
-
-	p := polok.Pipeline{
-		Rate:         expectedRate,
-		WorkerNumber: numWorkers,
-		Client:       ts.Client(),
-	}
-	defer p.Stop()
-
-	input := make(chan *http.Request, numRequests)
-
-	for i := 0; i < numRequests; i++ {
-		req, err := http.NewRequest(method, url, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		input <- req
-	}
-
-	start := time.Now()
-
-	results := p.Run(input)
-
-	for i := 0; i < numRequests; i++ {
-		res := <-results
-		got := res.StatusCode
-		if got != want {
-			t.Fatalf("got %v, want %v", got, want)
-		}
-	}
-
-	stop := time.Now()
-	duration := stop.Sub(start).Seconds()
-
-	rate := float64(numRequests) / duration
-
-	if len(results) != 0 {
-		t.Fatalf("remaining %d messages in results channel", len(results))
-	}
-	if rate > expectedRate {
-		t.Fatalf("rate %v, expected %v", rate, expectedRate)
-	}
-}
-
-func TestSlicetoStream(t *testing.T) {
-	t.Parallel()
-	var inputRequests []*http.Request
-	numRequests := 3
 
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World !")
 	}))
 
-	method := "GET"
-	url := ts.URL
+	testCases := []struct {
+		name         string
+		requests     []string
+		expectedRate float64
+		client       *http.Client
+	}{
+		{name: "2 requests - 1 QPS", requests: []string{ts.URL, ts.URL}, expectedRate: 1.0, client: ts.Client()},
+	}
 
-	for i := 0; i < numRequests; i++ {
-		req, err := http.NewRequest(method, url, nil)
-		if err != nil {
-			t.Fatal(err)
+	for _, tc := range testCases {
+		var wg sync.WaitGroup
+
+		c := polok.RateLimitedHTTPClient{
+			Transport: tc.client.Transport,
 		}
-		inputRequests = append(inputRequests, req)
-	}
+		c.Rate = tc.expectedRate
 
-	resultStream := polok.SlicetoStream(inputRequests)
+		start := time.Now()
 
-	for i := 0; i < numRequests; i++ {
-		<-resultStream
-	}
+		wg.Add(len(tc.requests))
 
-	if len(resultStream) != 0 {
-		t.Fatalf("%v remaining messages in result stream", len(resultStream))
-	}
-}
-
-func TestStreamtoSlice(t *testing.T) {
-	t.Parallel()
-	numResponse := 3
-	stream := make(chan *http.Response, numResponse)
-
-	for i := 0; i < numResponse; i++ {
-		res, err := newResponse()
-		if err != nil {
-			t.Fatal(err)
+		for _, req := range tc.requests {
+			go func(req string) {
+				c.Get(req)
+				wg.Done()
+			}(req)
 		}
-		stream <- res
+
+		wg.Wait()
+
+		stop := time.Now()
+		duration := stop.Sub(start).Seconds()
+		effectiveRate := float64(len(tc.requests)) / duration
+
+		if effectiveRate > tc.expectedRate {
+			t.Fatalf("effective rate %.2f, expected %.2f", effectiveRate, tc.expectedRate)
+		}
 	}
 
-	out := polok.StreamtoSlice(stream, numResponse)
-
-	if len(out) != numResponse {
-		t.Fatalf("got slice of length %v, want %v", len(out), numResponse)
-	}
-}
-
-func newResponse() (*http.Response, error) {
-	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World !")
-	}))
-	defer ts.Close()
-
-	req, err := http.NewRequest("GET", ts.URL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := ts.Client().Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return resp, nil
 }
